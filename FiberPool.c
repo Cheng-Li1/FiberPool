7e63a1d4b41fc22f87f7c0cbe8dff5cf6637525b
Cheng <lichengchaoreng@gmail.com>
2024-01-11 10:35:23 +1100

    First commit to FiberPool

---
+++ FiberPool.c
@@ -0,0 +1,128 @@
+#include "FiberPool.h"
+#include <stdint.h>
+
+uint32_t SelectNextCoroutine(int32_t index);
+int32_t FindFromPool(Fiber_t Handle);
+
+// This store the register part for the thread that init the coroutine pool
+static uint8_t Registerpart[AArch64_RegsterPart];
+static uint32_t active_co = 0;
+
+/* State code for Coroutines */
+typedef enum state_code {
+    NOT_INIT = 0,
+    FREE,
+    ACTIVE,
+    BlOCKED,
+    READY,
+} state_t;
+
+/* State code for Locks */
+typedef enum Lock_state {
+    EMPTY = 0,
+    CREATED,
+    AQUIRED,
+} lock_state_t;
+
+typedef struct Coroutine {
+    Fiber_t handle;
+    uint16_t priority;
+    state_t state;
+    uint32_t index;
+    struct stack_mem stack;
+} Coroutine_t;
+
+// The implementation of lock for coroutines does not involves atomics
+struct CoroutineLock {
+    lock_state_t state;
+    uint32_t index : 28;
+};
+
+// Well, we do not have malloc so I use static memory
+static Coroutine_t pool[MAX_COROUTINE_NUM];
+
+static struct CoroutineLock Lock[MAX_LOCK];
+
+int32_t FindFromPool(Fiber_t Handle) {
+    for (uint32_t i = 0; i < MAX_COROUTINE_NUM; i++) {
+        if (pool[i].handle == Handle) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+uint32_t SelectNextCoroutine(int32_t index) {
+    uint32_t i = 1, ret = 0;
+    Coroutine_t next;
+    next.priority = 0;
+    while (i < MAX_COROUTINE_NUM) {
+        if (pool[(index + i) % MAX_COROUTINE_NUM].state == READY && 
+              pool[(index + i) % MAX_COROUTINE_NUM].priority > next.priority) {
+            next = pool[index + i];
+            ret = (index + i) % MAX_COROUTINE_NUM;
+        }
+        i++;
+    }
+    return ret;
+}
+
+void yield() {
+    pool[active_co].state = READY;
+    uint32_t ret = SelectNextCoroutine(active_co);
+    pool[ret].state = ACTIVE;
+    active_co = ret;
+    Fiber_switch(pool[ret].handle);
+}
+
+void block() {
+    pool[active_co].state = BlOCKED;
+    uint32_t ret = SelectNextCoroutine(active_co);
+    pool[ret].state = ACTIVE;
+    active_co = ret;
+    Fiber_switch(pool[ret].handle);
+}
+
+void kill() {
+    pool[active_co].state = FREE;
+    uint32_t ret = SelectNextCoroutine(active_co);
+    pool[ret].state = ACTIVE;
+    active_co = ret;
+    Fiber_switch(pool[ret].handle);
+}
+
+void wake(co_handle index) {
+    pool[index].state = READY;
+}
+
+int32_t init(struct stack_mem* stack, uint32_t num, uint16_t priority) {
+    if (num > MAX_COROUTINE_NUM) {
+        return -1;
+    }
+    Fiber_init((Fiber_t)Registerpart);
+    pool[0].handle = (Fiber_t)Registerpart;
+    pool[0].priority = priority;
+    pool[0].state = READY;
+    for (int i = 1; i < num + 1; i++) {
+        pool[i].stack = stack[i - 1];
+        pool[i].state = FREE;
+    }
+    return 0;
+}
+
+int32_t push(void (*func)(void), void* args, uint16_t priority) {
+    int32_t i;
+    for (i = 0; i < MAX_COROUTINE_NUM; i++) {
+        if (pool[i].state == FREE) {
+            break;
+        }
+    }
+    if (i == MAX_COROUTINE_NUM) {
+        return -1;
+    }
+    Fiber_t handle = Fiber_create(pool[i].stack.memory, pool[i].stack.size, func);
+    Fiber_setargs(handle, args);
+    pool[i].priority = priority;
+    pool[i].state = READY;
+    return i;
+}
---
+++ FiberPool.h
@@ -0,0 +1,38 @@
+#define MAX_COROUTINE_NUM 5
+#define MAX_LOCK 16
+
+#include "FiberFlow/FiberFlow.h"
+#include <stdint.h>
+
+typedef struct CoroutineLock* mutex_t;
+typedef int32_t co_handle;
+
+struct stack_mem {
+    void* memory;
+    uint64_t size;
+};
+
+int32_t init(struct stack_mem* stack, uint32_t num, uint16_t priority);
+
+void wake(co_handle index);
+
+// Giving control to other coroutine
+void yield();
+
+void block();
+
+// Declare this coroutine is finished
+void kill();
+
+// Push a fiber into the coroutine pool
+// Return < 0 if there is no space
+co_handle push(void (* func)(void), void* args, uint16_t priority);
+
+mutex_t create_mutex();
+
+// Does not need a return value
+void acquire_mutex(mutex_t);
+
+void release_mutex(mutex_t);
+
+void destroy_mutex(mutex_t);
